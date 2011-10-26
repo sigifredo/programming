7b4c46e5b1ce99748e77f79cff79566dbf770c84
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2011-10-26 02:35:40 -0500

    32-ANeedleInTheHaystack en C

--- 32-ANeedleInTheHaystack/nhay.c
+++ 32-ANeedleInTheHaystack/nhay.c
@@ -1,45 +1,46 @@
 
 // Problem link: http://www.spoj.pl/problems/NHAY/
 
-#include<cstring>
-#include<cstdio>
+#include<string.h>
+#include<stdio.h>
 
 
-bool readInt(int &n)
+int readInt(int * n)
 {
     int e;
-    n = 0;
-    while(e = std::getchar())
+    *n = 0;
+    while(e = getchar())
     {
         if(e == '\n') return 1;
         else if(e == EOF) return 0;
-        n = (n * 10) + (e-48);
+        *n = (*n * 10) + (e-48);
     }
     return 0;
 }
 
-bool readChar(char &c)
+int readChar(char * c)
 {
-    c = std::getchar();
-    if(c == EOF || c == '\n') return false;
-    else return true;
+    *c = getchar();
+    if(*c == EOF || *c == '\n') return 0;
+    else return 1;
 }
 
 int main()
 {
     int n;
 
-    while(readInt(n))
+    while(readInt(&n))
     {
-        int b[n];
+        int b[n], i, q;
         char needle[n+1];
 
-        std::memset(b, 0, n*sizeof(int));
-        std::memset(needle, '\0', (n+1)*sizeof(char));
+        memset(b, 0, n*sizeof(int));
+        memset(needle, '\0', (n+1)*sizeof(char));
 
-        for(int i = 0; i < n; i++) readChar(needle[i]); std::getchar();
+        for(i = 0; i < n; i++) readChar(&needle[i]);
+        getchar();
 
-        for(int i = 1, q = 0; i < n; i++)
+        for(i = 1, q = 0; i < n; i++)
         {
             if(needle[i] == needle[q]) q++;
             else q = 0;
@@ -47,15 +48,16 @@ int main()
         }
 
         char c;
-        int i = 0, q = 0, e = 0;
-        while(readChar(c))
+        int e = 0;
+        i = q = 0;
+        while(readChar(&c))
         {
             if(c == needle[q])
             {
                 q++;
                 if(q == n)
                 {
-                    std::printf("%d\n", (i-n+1));
+                    printf("%d\n", (i-n+1));
                     q = b[q-1];
                     e++;
                 }
@@ -67,7 +69,7 @@ int main()
             }
             i++;
         }
-        if(e == 0) std::puts("");
+        if(e == 0) puts("");
     }
 
     return 0;
