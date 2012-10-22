6d400769dd34783e93f3ddfc2dcdf61a56a7c72c
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-10-21 19:06:27 -0500

    Terminamos el punto a en MIPS

--- 2012-2-LabArq/a/a.asm
+++ 2012-2-LabArq/a/a.asm
@@ -1,6 +1,6 @@
 
 .data
-    numeros: .word 60, 2, 3, 4, 5, 6, 7, 8, 69
+    numeros: .word 8, 2, 3, 4, 5, 6, 7, 8, 9
 
 .text
     la $a0, numeros # leemos la dirección del arreglo.
@@ -17,14 +17,13 @@
     syscall
 
 mayores:
-    li $t0, 0 # i
-    li $t1, 0 # contador
-    
-    loop:
-    	sll $t3, $t1, 2 # $t3 = v[i]
+    addi $t0, $zero, 0 # i
+    addi $t1, $zero, 0 # contador
 
-    	# if(v[i] > v[0])
-    	lw $t5, 0($a0)
+    loop:
+    	sll $t3, $t0, 2 # $t3 = v[i]
+
+        lw $t5, 0($a0)
 
         add $t6, $a0, $t3
         lw $t6, 0($t6)
@@ -33,8 +32,8 @@ mayores:
     	    addi $t1, $t1, 1 # contador++;
     	endif:
 
-	addi $t0, $t0, 1 # i++;
-	bne $a1, $t1, loop  # si i != n, salte a loop
+	addi $t0, $t0, 1 # i++
+	bne $a1, $t0, loop  # si i != n, salte a loop
     endl:
 
     add $v0, $t1, $zero
